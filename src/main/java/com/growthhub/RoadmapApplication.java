a070e12239447bff3ce320a916a6fee1695db6bf
MoonMinHyuk1 <moon981018@gmail.com>
2024-09-26 00:02:55 +0900

    chore: 경로 변경

--- src/main/java/com/growthhub/RoadmapApplication.java
+++ src/main/java/com/growthhub/RoadmapApplication.java
@@ -1,17 +1,14 @@
-package com.growthhub.roadmap;
+package com.growthhub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 
-@EnableFeignClients(basePackages = "com.growthhub.global.client")
+@EnableFeignClients
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.growthhub.roadmap", "com.growthhub.post"})
 public class RoadmapApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RoadmapApplication.class, args);
 	}
-
 }
